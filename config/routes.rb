b8d67f4fc80144400d19ed901a95ff8c15980811
tatsuya1013 <tatsuya101344@icloud.com>
2019-03-09 18:10:41 +0900

    Create-view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'messages#index'
   resources :users, only:[:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-  resouces :messages, only: [:index, :create]
+  resources :messages, only: [:index, :create]
+  end
 end
