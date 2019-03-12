9715bc1242b5d8698d932cd4987b698f27e74b24
tatsuya1013 <tatsuya101344@icloud.com>
2019-03-12 19:18:32 +0900

    Create-messagemodel

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,4 @@
 class GroupsController < ApplicationController
-
   def index
   end
 
@@ -17,14 +16,21 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids =>[] })
+    params.require(:group).permit(:name, user_ids: [] )
+  end
+
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
